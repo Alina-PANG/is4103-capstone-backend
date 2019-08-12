23e99463a960735db4658c18fcfb6f58b06d98ce
Pang Hangzhi <hangzhipang@gmail.com>
2019-08-12 17:27:55 +0800

    spring boot backend initialization

---
+++ src/main/java/capstone/is4103capstone/Is4103CapstoneApplication.java
@@ -0,0 +1,13 @@
+package capstone.is4103capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is4103CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is4103CapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/is4103capstone/Is4103CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.is4103capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is4103CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
