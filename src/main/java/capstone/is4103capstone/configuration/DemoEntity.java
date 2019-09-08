e3650ed2ab0e83946ea84a8a315b476972ca92ea
Yingshi <huangyingshi@gmail.com>
2019-09-08 16:57:06 +0800

    Database Configuration - entity template

---
+++ src/main/java/capstone/is4103capstone/configuration/DemoEntity.java
@@ -0,0 +1,12 @@
+package capstone.is4103capstone.configuration;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//add the two annotations
+//@Entity
+//@Table
+public class DemoEntity extends DBEntityTemplate {
+    //other properties of the entity
+    //+getter/setter
+}
