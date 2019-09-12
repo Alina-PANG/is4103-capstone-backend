24b623acb561291659e8b303cca0c4f288996cb2
Yingshi <huangyingshi@gmail.com>
2019-09-12 22:07:24 +0800

    project

---
+++ src/main/java/capstone/is4103capstone/entities/finance/Project.java
@@ -0,0 +1,90 @@
+package capstone.is4103capstone.entities.finance;
+
+import capstone.is4103capstone.configuration.DBEntityTemplate;
+import capstone.is4103capstone.entities.Employee;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+public class Project extends DBEntityTemplate {
+
+
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "projectOwner_Id")
+    private Employee projectOwner;
+
+    private String description;
+
+    private Double budgetAmt;
+
+    private String costCenterCode;
+
+    public Project() {
+    }
+
+    public Project(String projectName, String projectCode, Date startDate, Date endDate, String description, Double budgetAmt,String costCenterCode) {
+        super(projectName, projectCode);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.budgetAmt = budgetAmt;
+        this.costCenterCode = costCenterCode;
+    }
+
+
+
+    public String getCostCenterCode() {
+        return costCenterCode;
+    }
+
+    public void setCostCenterCode(String costCenterCode) {
+        this.costCenterCode = costCenterCode;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Employee getProjectOwner() {
+        return projectOwner;
+    }
+
+    public void setProjectOwner(Employee projectOwner) {
+        this.projectOwner = projectOwner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getBudgetAmt() {
+        return budgetAmt;
+    }
+
+    public void setBudgetAmt(Double budgetAmt) {
+        this.budgetAmt = budgetAmt;
+    }
+}
