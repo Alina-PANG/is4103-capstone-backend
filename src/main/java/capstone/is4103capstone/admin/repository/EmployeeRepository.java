191489704a7d35b8c90fa9c179d90a7db2cb2387
Ryan Sim <simweijie@gmail.com>
2019-09-15 12:01:36 +0800

    EmployeeRepository - add findEmployeeByUserName

--- src/main/java/capstone/is4103capstone/admin/repository/EmployeeRepository.java
+++ src/main/java/capstone/is4103capstone/admin/repository/EmployeeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee,String> {
 
+    public Employee findEmployeeByUserName(String username);
 
 
 }
