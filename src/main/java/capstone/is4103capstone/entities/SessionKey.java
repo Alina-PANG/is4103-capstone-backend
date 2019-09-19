a7e5ad3cb5b090240009eb8022078e4d4c0d09ce
Ryan Sim <simweijie@gmail.com>
2019-09-19 12:56:48 +0800

    SessionKey entity - fixes to data model
    Added auto increment to id

--- src/main/java/capstone/is4103capstone/entities/SessionKey.java
+++ src/main/java/capstone/is4103capstone/entities/SessionKey.java
@@ -1,9 +1,6 @@
 package capstone.is4103capstone.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -11,6 +8,7 @@ import java.util.Date;
 public class SessionKey {
 
     @Id
+    @GeneratedValue
     private int id;
     private String sessionKey;
     private Date lastAuthenticated;
