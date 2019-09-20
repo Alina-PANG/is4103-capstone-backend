94ffa259a49fd91c62329b70d6d305b095f01bc2
Yingshi <huangyingshi@gmail.com>
2019-09-20 08:30:50 +0800

    create cat

--- src/main/java/capstone/is4103capstone/finance/admin/controller/BudgetCategoryController.java
+++ src/main/java/capstone/is4103capstone/finance/admin/controller/BudgetCategoryController.java
@@ -1,15 +1,43 @@
 package capstone.is4103capstone.finance.admin.controller;
 
-import capstone.is4103capstone.entities.finance.BudgetSub1;
+import capstone.is4103capstone.finance.admin.model.req.CreateBudgetCategoryRequest;
+import capstone.is4103capstone.finance.admin.model.res.BudgetCategoryRes;
+import capstone.is4103capstone.finance.admin.service.CategoryService;
+import capstone.is4103capstone.general.Authentication;
+import capstone.is4103capstone.general.DefaultData;
+import capstone.is4103capstone.general.model.GeneralRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/category")
+@RequestMapping("/api/fin/category")
 public class BudgetCategoryController {
     private static final Logger logger = LoggerFactory.getLogger(BudgetCategoryController.class);
+
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/create")
+    public ResponseEntity<BudgetCategoryRes> createCategory(@RequestBody CreateBudgetCategoryRequest categoryRequest) {
+        if(Authentication.authenticateUser(categoryRequest.getUsername()))
+            return ResponseEntity
+                    .ok()
+                    .body(categoryService.createCategory(categoryRequest));
+        else
+            return ResponseEntity
+                    .badRequest()
+                    .body(new BudgetCategoryRes(DefaultData.AUTHENTICATION_ERROR_MSG, true,null));
+    }
+
+
+
+
 
 
 
--- src/main/java/capstone/is4103capstone/finance/admin/service/CategoryService.java
+++ src/main/java/capstone/is4103capstone/finance/admin/service/CategoryService.java
@@ -1,10 +1,90 @@
 package capstone.is4103capstone.finance.admin.service;
 
+import capstone.is4103capstone.admin.repository.CountryRepository;
+import capstone.is4103capstone.configuration.DBEntityTemplate;
+import capstone.is4103capstone.entities.Country;
+import capstone.is4103capstone.entities.finance.BudgetCategory;
+import capstone.is4103capstone.finance.Repository.BudgetCategoryRepository;
+import capstone.is4103capstone.finance.admin.model.CategoryModel;
+import capstone.is4103capstone.finance.admin.model.req.CreateBudgetCategoryRequest;
+import capstone.is4103capstone.finance.admin.model.res.BudgetCategoryRes;
+import capstone.is4103capstone.general.Authentication;
+import capstone.is4103capstone.general.model.GeneralRes;
+import capstone.is4103capstone.util.FinanceEntityCodeHPGenerator;
+import capstone.is4103capstone.util.exception.RepositoryEntityMismatchException;
+import javafx.scene.chart.ScatterChart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
     private static final Logger logger = LoggerFactory.getLogger(CategoryService.class);
+
+    @Autowired
+    BudgetCategoryRepository budgetCategoryRepository;
+    @Autowired
+    CountryRepository countryRepository;
+
+
+    public BudgetCategoryRes createCategory(CreateBudgetCategoryRequest categoryRequest){
+        try {
+            Country country = countryRepository.findCountryByCode(categoryRequest.getCountryCode());
+            if (country == null) {
+                return new BudgetCategoryRes("Country Code does not exist!", true, null);
+            }
+
+            BudgetCategory newCat = new BudgetCategory(categoryRequest.getCategoryName());
+            newCat.setCreatedBy(categoryRequest.getUsername());
+            newCat.setHierachyPath(g.generateHierachyPath(country, newCat));
+
+            Authentication.configurePermissionMap(newCat);
+
+            newCat = budgetCategoryRepository.save(newCat);
+            newCat.setCountry(country);
+            newCat.setCode(generateCode(budgetCategoryRepository, newCat));
+
+            //below: unmanaged/change for reponse sending
+//        newCat.setPermissionMap(null);
+//        newCat.setBudgetSub1s(null);
+
+            return new BudgetCategoryRes("Successfully created new category!", false, new CategoryModel(newCat.getObjectName(), newCat.getCode()));
+        }catch (Exception e){
+            return new BudgetCategoryRes(e.getMessage(),true,null);
+        }
+    }
+
+//    public BudgetCategoryRes createCategory2(CreateBudgetCategoryRequest categoryRequest){
+//        Country country = countryRepository.findCountryByCode(categoryRequest.getCountryCode());
+//        if (country == null){
+//            return new BudgetCategoryRes("Country Code does not exist!",true,null);
+//        }
+//
+//        BudgetCategory newCat = new BudgetCategory(categoryRequest.getCategoryName());
+//        newCat.setCreatedBy(categoryRequest.getUsername());
+//        newCat.setHierachyPath(g.generateHierachyPath(country,newCat));
+//
+//        Authentication.configurePermissionMap(newCat);
+//
+//        newCat = budgetCategoryRepository.save(newCat);
+//        newCat.setCountry(country);
+//        newCat.setCode(generateCode(budgetCategoryRepository,newCat));
+//
+//
+//
+//        return new BudgetCategoryRes("Successfully created new category!",false,new Ca);
+//    }
+
+    FinanceEntityCodeHPGenerator g = new FinanceEntityCodeHPGenerator();
+    private String generateCode(JpaRepository repo, DBEntityTemplate entity){
+        try {
+            return g.generateCode(repo,entity);
+        }catch (RepositoryEntityMismatchException e){
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
