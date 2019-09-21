2b46f41c708f967aa6f263fe5fabb35bad53d4f6
Yingshi <huangyingshi@gmail.com>
2019-09-22 00:15:09 +0800

    merchandise

--- src/main/java/capstone/is4103capstone/finance/Repository/MerchandiseRepository.java
+++ src/main/java/capstone/is4103capstone/finance/Repository/MerchandiseRepository.java
@@ -2,9 +2,16 @@ package capstone.is4103capstone.finance.Repository;
 
 import capstone.is4103capstone.entities.finance.Merchandise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MerchandiseRepository extends JpaRepository<Merchandise, String> {
     public Merchandise findMerchandiseByCode(String merchandiseCode);
+
+    @Query(value = "SELECT * FROM merchandise m WHERE m.is_deleted=false AND m.budget_sub2_id=?1",nativeQuery = true)
+    public List<Merchandise> findMerchandiseByBudgetSub2Id(String sub2Id);
+
+    @Query(value = "SELECT * FROM merchandise m WHERE m.is_deleted=false AND m.vendor_id=?1", nativeQuery = true)
+    public List<Merchandise> findMerchandiseByVendorId(String vendorId);
 }
