dc6d03b869bc3cbe6384dd10806174708bc41f0d
Yingshi <huangyingshi@gmail.com>
2019-09-26 15:52:14 +0800

    cc

--- src/main/java/capstone/is4103capstone/admin/model/CostCenterModel.java
+++ src/main/java/capstone/is4103capstone/admin/model/CostCenterModel.java
@@ -11,7 +11,6 @@ public class CostCenterModel implements Serializable {
     private String ccManagerId;
 
 
-
     public CostCenterModel() {
     }
 
---
+++ src/main/java/capstone/is4103capstone/admin/model/res/GetCostCentersRes.java
@@ -0,0 +1,13 @@
+package capstone.is4103capstone.admin.model.res;
+
+import capstone.is4103capstone.admin.model.CostCenterModel;
+import capstone.is4103capstone.general.model.GeneralRes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetCostCentersRes extends GeneralRes {
+    List<CostCenterModel> costCentersList = new ArrayList<>();
+
+
+}
