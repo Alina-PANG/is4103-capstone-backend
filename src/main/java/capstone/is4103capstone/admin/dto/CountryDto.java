ad1e7faf0d71c7d876f2785bcd231b286fc81a97
Yingshi <huangyingshi@gmail.com>
2019-10-04 22:24:25 +0800

    asdf

--- src/main/java/capstone/is4103capstone/admin/dto/CountryDto.java
+++ src/main/java/capstone/is4103capstone/admin/dto/CountryDto.java
@@ -8,8 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
-@Getter
-@Setter
+
 @JsonInclude(JsonInclude.Include.NON_NULL) // don't serialize nulls since there are optional classes
 public class CountryDto implements Serializable {
 
@@ -18,4 +17,44 @@ public class CountryDto implements Serializable {
     private Optional<String> code = Optional.empty();
     private Optional<String> regionId = Optional.empty();
     private Optional<List<String>> functions = Optional.empty();
+
+    public Optional<String> getId() {
+        return id;
+    }
+
+    public void setId(Optional<String> id) {
+        this.id = id;
+    }
+
+    public Optional<String> getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(Optional<String> objectName) {
+        this.objectName = objectName;
+    }
+
+    public Optional<String> getCode() {
+        return code;
+    }
+
+    public void setCode(Optional<String> code) {
+        this.code = code;
+    }
+
+    public Optional<String> getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(Optional<String> regionId) {
+        this.regionId = regionId;
+    }
+
+    public Optional<List<String>> getFunctions() {
+        return functions;
+    }
+
+    public void setFunctions(Optional<List<String>> functions) {
+        this.functions = functions;
+    }
 }
