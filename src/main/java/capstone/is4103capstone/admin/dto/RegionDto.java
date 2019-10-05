dabe51064e90b7078192dcbb93a297c984777935
Yingshi <huangyingshi@gmail.com>
2019-10-05 08:19:35 +0800

    function id with name

--- src/main/java/capstone/is4103capstone/admin/dto/RegionDto.java
+++ src/main/java/capstone/is4103capstone/admin/dto/RegionDto.java
@@ -8,8 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
-@Getter
-@Setter
+
 @JsonInclude(JsonInclude.Include.NON_NULL) // don't serialize nulls since there are optional classes
 public class RegionDto implements Serializable {
 
@@ -18,4 +17,35 @@ public class RegionDto implements Serializable {
     public Optional<String> code = Optional.empty();
     public Optional<List<String>> countryIds = Optional.empty();
 
+    public Optional<String> getId() {
+        return id;
+    }
+
+    public void setId(Optional<String> id) {
+        this.id = id;
+    }
+
+    public Optional<String> getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(Optional<String> objectName) {
+        this.objectName = objectName;
+    }
+
+    public Optional<String> getCode() {
+        return code;
+    }
+
+    public void setCode(Optional<String> code) {
+        this.code = code;
+    }
+
+    public Optional<List<String>> getCountryIds() {
+        return countryIds;
+    }
+
+    public void setCountryIds(Optional<List<String>> countryIds) {
+        this.countryIds = countryIds;
+    }
 }
