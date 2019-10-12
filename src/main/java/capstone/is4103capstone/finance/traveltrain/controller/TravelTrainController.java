84652b3ab4c50f17e9bc61af5b305d61e5187349
Yingshi <huangyingshi@gmail.com>
2019-10-12 21:11:53 +0800

    api

--- src/main/java/capstone/is4103capstone/finance/traveltrain/controller/TravelTrainController.java
+++ src/main/java/capstone/is4103capstone/finance/traveltrain/controller/TravelTrainController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/fin-item")
+@RequestMapping("/api/tt")
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TravelTrainController {
