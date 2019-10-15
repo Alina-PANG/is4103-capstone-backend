df6503079fb69a7ae7b4c69d9956408eac587dd7
Ryan Sim <simweijie@gmail.com>
2019-10-16 00:43:18 +0800

    Attempt to standardize error messages across components v1

---
+++ src/main/java/capstone/is4103capstone/general/StandardStatusMessages.java
@@ -0,0 +1,14 @@
+package capstone.is4103capstone.general;
+
+public class StandardStatusMessages {
+
+    public static String OPERATION_COMPLETED_SUCCESSFULLY = "The operation completed successfully.";
+    public static String NO_SEARCH_RESULTS_FOUND = "Sorry, I could not find any results based on your search query.";
+    public static String NO_RECORDS_IN_DB = "Sorry, there were no records to retrieve from the database.";
+    public static String NO_NONDELETED_RECORDS_FOUND = "Sorry, I could not find any active (non-deleted) records based on your search query.";
+    public static String INVALID_USERNAME_PASSWORD = "Oops! The username or password you entered was not correct. Please try again, or contact your system administrator.";
+    public static String INVALID_INPUT = "Sorry, I couldn't understand your input. Please check and try again.";
+    public static String INVALID_SESSION_KEY = "Sorry, the session key you provided was invalid, or your session has already timed out. Please log in again.";
+    public static String NO_RIGHTS = "Sorry, it doesn't seem that you have the necessary permissions to perform this operation.";
+
+}
