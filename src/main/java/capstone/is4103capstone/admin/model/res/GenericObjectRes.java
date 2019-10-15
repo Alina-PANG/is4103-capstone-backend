f3fdc7f0358692c4df9a3fcac438f1d60cec29eb
Ryan Sim <simweijie@gmail.com>
2019-10-15 20:00:07 +0800

    Implement Generic Object Response to supersede custom entity responses

---
+++ src/main/java/capstone/is4103capstone/admin/model/res/GenericObjectRes.java
@@ -0,0 +1,30 @@
+package capstone.is4103capstone.admin.model.res;
+
+import capstone.is4103capstone.admin.dto.EmployeeDto;
+import capstone.is4103capstone.general.model.GeneralRes;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Optional;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_EMPTY) // don't serialize nulls since there are optional classes
+public class GenericObjectRes extends GeneralRes {
+
+    public Integer totalRecords;
+    private Optional<List<Object>> results;
+
+    public GenericObjectRes() {
+    }
+
+    public GenericObjectRes(String message, Boolean hasError, Optional<List<Object>> results) {
+        super(message, hasError);
+        this.results = results;
+        results.ifPresent(obj -> {
+            totalRecords = obj.size();
+        });
+    }
+}
