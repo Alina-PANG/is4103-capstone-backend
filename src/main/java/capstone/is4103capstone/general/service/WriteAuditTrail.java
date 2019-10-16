e76980e487b5220bdf8fd4fc4ad1f00835b755d5
Ryan Sim <simweijie@gmail.com>
2019-10-16 10:41:17 +0800

    autoAuditUsingSpringSecurity() function

--- src/main/java/capstone/is4103capstone/general/service/WriteAuditTrail.java
+++ src/main/java/capstone/is4103capstone/general/service/WriteAuditTrail.java
@@ -1,55 +1,66 @@
 package capstone.is4103capstone.general.service;
 
 import capstone.is4103capstone.admin.service.AuditTrailActivityService;
-import capstone.is4103capstone.configuration.DBEntityTemplate;
+import capstone.is4103capstone.entities.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WriteAuditTrail {
     private static final Logger logger = LoggerFactory.getLogger(WriteAuditTrail.class);
-
+    private static final String AUTO_DETECT_FLAG = "autodetect";
+    static AuditTrailActivityService audit;
     @Autowired
     AuditTrailActivityService auditService;
 
+    public static void autoAudit(String username) {
+        try {
+            audit.createNewRecordUsingUsername(AUTO_DETECT_FLAG, username);
+        } catch (Exception e) {
+            logger.error("Audit function error, username given from the internal controller is not correct. ");
+        }
+    }
+
+    public static void autoAuditUsingSpringSecurity() {
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            Employee user = (Employee) auth.getPrincipal();
+            audit.createNewRecordUsingUsername(AUTO_DETECT_FLAG, user.getUserName());
+        } catch (Exception ex) {
+            logger.error("Audit function error, username given from the internal controller is not correct. ");
+        }
+    }
+
+    public static void autoAuditRecordByUserId(String uuid) {
+        try {
+            audit.createNewRecordUsingUserUuid(AUTO_DETECT_FLAG, uuid);
+        } catch (Exception e) {
+            logger.error("Audit function error, username given from the internal controller is not correct. ");
+        }
+    }
+
+    public static void auditActivity(String activityDescription, String username) {
+        try {
+            audit.createNewRecordUsingUsername(activityDescription, username);
+        } catch (Exception e) {
+            logger.error("Audit function error, username given from the internal controller is not correct. ");
+        }
+    }
+
+    public static void auditActivityRecordByUserId(String activityDescription, String uuid) {
+        try {
+            audit.createNewRecordUsingUserUuid(activityDescription, uuid);
+        } catch (Exception e) {
+            logger.error("Audit function error, username given from the internal controller is not correct. ");
+        }
+    }
+
     @Autowired
-    public void setAudit(AuditTrailActivityService service){
+    public void setAudit(AuditTrailActivityService service) {
         WriteAuditTrail.audit = service;
-    }
-
-    static AuditTrailActivityService audit;
-    private static final String AUTO_DETECT_FLAG = "autodetect";
-
-    public static void autoAudit(String username){
-        try {
-            audit.createNewRecordUsingUsername(AUTO_DETECT_FLAG,username);
-        }catch (Exception e){
-            logger.error("Audit function error, username given from the internal controller is not correct. ");
-        }
-    }
-
-
-    public static void autoAuditRecordByUserId(String uuid){
-        try {
-            audit.createNewRecordUsingUserUuid(AUTO_DETECT_FLAG,uuid);
-        }catch (Exception e){
-            logger.error("Audit function error, username given from the internal controller is not correct. ");
-        }
-    }
-    public static void auditActivity(String activityDescription, String username){
-        try {
-            audit.createNewRecordUsingUsername(activityDescription,username);
-        }catch (Exception e){
-            logger.error("Audit function error, username given from the internal controller is not correct. ");
-        }
-    }
-    public static void auditActivityRecordByUserId(String activityDescription, String uuid){
-        try {
-            audit.createNewRecordUsingUserUuid(activityDescription,uuid);
-        }catch (Exception e){
-            logger.error("Audit function error, username given from the internal controller is not correct. ");
-        }
     }
 }
