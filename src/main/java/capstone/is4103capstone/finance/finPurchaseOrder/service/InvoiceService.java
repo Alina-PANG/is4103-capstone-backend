2038c89b59c712c77c20b65acdfc15cca430bcce
Pang Hangzhi <hangzhipang@gmail.com>
2019-10-24 15:25:14 +0800

    invoice controller and services

--- src/main/java/capstone/is4103capstone/finance/finPurchaseOrder/service/InvoiceService.java
+++ src/main/java/capstone/is4103capstone/finance/finPurchaseOrder/service/InvoiceService.java
@@ -1,12 +1,19 @@
 package capstone.is4103capstone.finance.finPurchaseOrder.service;
 
 import capstone.is4103capstone.entities.finance.Invoice;
+import capstone.is4103capstone.entities.finance.PurchaseOrder;
+import capstone.is4103capstone.entities.finance.StatementOfAcctLineItem;
 import capstone.is4103capstone.entities.supplyChain.OutsourcingAssessment;
 import capstone.is4103capstone.entities.supplyChain.OutsourcingAssessmentLine;
 import capstone.is4103capstone.entities.supplyChain.OutsourcingAssessmentSection;
+import capstone.is4103capstone.entities.supplyChain.Vendor;
 import capstone.is4103capstone.finance.Repository.InvoiceRepository;
+import capstone.is4103capstone.finance.Repository.PurchaseOrderRepository;
+import capstone.is4103capstone.finance.Repository.StatementOfAccountLineItemRepository;
+import capstone.is4103capstone.finance.finPurchaseOrder.model.req.CreateInvoiceReq;
 import capstone.is4103capstone.finance.finPurchaseOrder.model.res.GetInvoiceRes;
 import capstone.is4103capstone.general.model.GeneralRes;
+import capstone.is4103capstone.supplychain.Repository.VendorRepository;
 import capstone.is4103capstone.supplychain.outsourcing.assessmentForm.controller.AssessmentFormController;
 import capstone.is4103capstone.supplychain.outsourcing.assessmentForm.model.res.GetAssessmentFormRes;
 import capstone.is4103capstone.util.exception.FileStorageException;
@@ -28,6 +35,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -35,25 +43,61 @@ public class InvoiceService {
     private static final Logger logger = LoggerFactory.getLogger(InvoiceService.class);
     @Autowired
     InvoiceRepository invoiceRepository;
-    public ResponseEntity<GeneralRes> storeFile(MultipartFile file) {
+    @Autowired
+    StatementOfAccountLineItemRepository statementOfAccountLineItemRepository;
+    @Autowired
+    VendorRepository vendorRepository;
 
-        // Normalize file name
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-
+    public ResponseEntity<GeneralRes> update(CreateInvoiceReq createInvoiceReq, String soaId, String username) {
         try {
-            // Check if the file's name contains invalid characters
-//            if(fileName.contains("..")) {
-//                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
-//            }
-            Invoice invoice= new Invoice();
+            StatementOfAcctLineItem soa = statementOfAccountLineItemRepository.getOne(soaId);
+            Vendor vendor = vendorRepository.getOne(createInvoiceReq.getVendorId());
+            if(soa == null || vendor == null) return ResponseEntity.notFound().build();
+
+            Invoice invoice = soa.getInvoice();
+            if(invoice == null) invoice = new Invoice();
+
+            invoice.setCurrencyCode(createInvoiceReq.getCurrencyCode());
+            invoice.setDescription(createInvoiceReq.getDescription());
+            invoice.setPaymentAmount(createInvoiceReq.getTotalAmt());
+            invoice.setVendor(vendor);
+            invoice.setStatementOfAcctLineItem(soa);
+
+            invoice = invoiceRepository.saveAndFlush(invoice);
+            soa.setInvoice(invoice);
+            statementOfAccountLineItemRepository.saveAndFlush(soa);
+            logger.info("Successfully updated the invoice!");
+            return ResponseEntity.ok().body(new GeneralRes("Successfully updated the invoice information!", false));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return ResponseEntity
+                    .badRequest()
+                    .body(new GeneralRes("An unexpected error has occured: "+ ex.toString(), true));
+        }
+    }
+
+    public ResponseEntity<GeneralRes> storeFile(MultipartFile file, String username, String soaId) {
+        try {
+            StatementOfAcctLineItem soa = statementOfAccountLineItemRepository.getOne(soaId);
+            if(soa == null) return ResponseEntity.notFound().build();
+
+            Invoice invoice = soa.getInvoice();
+            if(invoice == null) invoice = new Invoice();
+
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
             invoice.setFileName(fileName);
             invoice.setFileType(file.getContentType());
             invoice.setData(file.getBytes());
+            invoice.setCreatedBy(username);
+            invoice.setCreatedDateTime(new Date());
+            invoice.setStatementOfAcctLineItem(soa);
 
-            invoiceRepository.save(invoice);
+            invoice = invoiceRepository.saveAndFlush(invoice);
+            soa.setInvoice(invoice);
+            statementOfAccountLineItemRepository.saveAndFlush(soa);
             logger.info("Successfully saved the invoice!");
             return ResponseEntity.ok().body(new GeneralRes("Successfully uploaded the invoice!", false));
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
             return ResponseEntity
                     .badRequest()
@@ -63,7 +107,7 @@ public class InvoiceService {
 
     public ResponseEntity<Resource> getFile(String fileId) {
         try{
-            Invoice invoice= invoiceRepository.getOne(fileId);
+            Invoice invoice = invoiceRepository.getOne(fileId);
             if(invoice == null) return ResponseEntity
                     .notFound().build();
             Resource resource = new ByteArrayResource(invoice.getData());
